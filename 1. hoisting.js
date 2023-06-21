9bd7428178d9fa03f96323e848fc1e5148fde35d
Jerald <jeraldtagadtad09@gmail.com>
2023-06-21 17:14:12 +0800

    second commit

---
+++ 1. hoisting.js
@@ -0,0 +1,15 @@
+// declaration of same variable name is possiible in Javascript 
+
+var a = 1;
+
+var a = 2; 
+
+console.log(a);
+
+// Hoisting is put a variable at the top of the file
+// javascript converted var to this format 
+
+var a;
+
+a = 1; 
+a = 2;
---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('hello');
+
+// ECMA SCRIPT (Original name of javascript)
+
+//Current Version of javascript is ES13
